cf0ae754b007142b4dda82841f0d56bdaf84dc0a
Felipe Fernandes <feliepbinfo@gmail.com>
2023-10-16 21:53:43 -0300

    :up: estilização de datas agendadas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,10 +13,26 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(DateTime? selecionada)
     {
-        return View();
+
+        var lista = new List<DateTime>()
+        {
+            // new DateTime(2023,10,16),
+            new DateTime(2023,10,17),
+            new DateTime(2023,10,18)
+        };
+        var data = selecionada ?? DateTime.Now;
+        if (selecionada != null)
+            lista.Add((DateTime)selecionada);
+        var viewModel = new TesteViewModel()
+        {
+            Data = data.ToString("dd/MM/yyyy"),
+            Datas = lista
+        };
+        return View(viewModel);
     }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
---
+++ Models/ViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bootstrap_datepicker.Models
+{
+    public class TesteViewModel
+    {
+        public string? Data { get; set; }
+        public List<DateTime>? Datas { get; set; }
+    }
+}
